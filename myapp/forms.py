39725f2bb9bdce55e8ae87de81f419d4c83d237e
Robel Kebede <robel.kebede@bison.howard.edu>
2023-09-29 19:24:19 -0400

    Add forms to the website

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,3 +1,13 @@
 from django.forms import ModelForm
-from .models import Booking
+from .models import Volunteer, Events
 
+class VolunteerForm(ModelForm):
+    class Meta:
+        model = Volunteer
+        fields = "__all__"
+
+
+class EventsForm(ModelForm):
+    class Meta:
+        model = Events
+        fields = "__all__"
