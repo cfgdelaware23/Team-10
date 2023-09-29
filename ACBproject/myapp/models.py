ae5ba89414278d61af0cad55cfabddfdda7e92e8
Robel Kebede <robel.kebede@bison.howard.edu>
2023-09-29 19:42:37 -0400

    Set default values to none for each table field

--- ACBproject/myapp/models.py
+++ ACBproject/myapp/models.py
@@ -2,26 +2,25 @@ from django.db import models
 
 
 class Volunteer(models.Model):
-    name = models.TextField()
-    day = models.DateField()
-    roles = models.TextField()
+    name = models.TextField(default=None)
+    day = models.DateField(default=None)
+    roles = models.TextField(default=None)
 
     def __str__(self) -> str:
         return f"Name: {self.name}, available day: {self.day}, roles: {self.roles}"
 
 
 class Events(models.Model):
-
-    recurring = models.BooleanField()
-    day = models.DateField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    title = models.TextField()
-    account = models.TextField()
-    host = models.TextField()
-    moderator = models.TextField()
-    facilitator = models.TextField()
-    broadcaster = models.TextField()
+    recurring = models.BooleanField(default=None)
+    day = models.DateField(default=None)
+    start_time = models.DateTimeField(default=None)
+    end_time = models.DateTimeField(default=None)
+    title = models.TextField(default=None)
+    account = models.TextField(default=None)
+    host = models.TextField(default=None)
+    moderator = models.TextField(default=None)
+    facilitator = models.TextField(default=None)
+    broadcaster = models.TextField(default=None)
 
     def __str__(self) -> str:
         return f"title: {self.title}, day: {self.day}, time: {self.time}, account: {self.account}"
