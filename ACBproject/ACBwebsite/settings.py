10d3fa5b2c52bc0d99dc242d160c0a96ae52a5b6
Juskeerat Anand <juskeerat@gmail.com>
2023-09-29 19:47:57 -0400

    created connection between frontend and backend
    connecting django and react

--- ACBproject/ACBwebsite/settings.py
+++ ACBproject/ACBwebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = "ACBwebsite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['myapp/templates'],
+        "DIRS": ['myapp/templates', os.path.join(BASE_DIR, 'client/bulid')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'client/bulid'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
