aa2f8bdd755e3976ebc2f42f7afcdd17f86dee3a
JorgeAGalvis <jag2425@columbia.edu>
2023-09-29 18:54:07 -0400

    create clear

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class VolunteerForm(models.Model):
+    name = models.CharField(max_length=25)
+    day = models.DateField()
+    roles = models.TextField()
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}, available day: {self.day}, roles: {self.roles}"
+
+
+class EventsForm(models.Model):
+
+    recurring = models.BooleanField()
+    day = models.DateField()
+    time = models.DateTimeField()
+    title = models.TextField()
+    account = models.TextField()
+
+    def __str__(self) -> str:
+        return f"title: {self.title}, day: {self.day}, time: {self.time}, account: {self.account}"
