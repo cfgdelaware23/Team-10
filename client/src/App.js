59c9d4cb38fcb96d7f804642beca447ec8e4d7a0
Jaehee Ashley <ja4840@princeton.edu>
2023-09-29 19:57:01 -0400

    trying to figure out routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,41 @@
 import './App.css';
 import volunteer from './volunteer.js';
-import user from './user.js';
+import './user.js';
 import admin from  './admin.js' ;
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link
 } from "react-router-dom";
 
 import './App.css';
-
-
-
+import User from './user.js';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+
         <p>
           Select Page
-
         </p>
-        
-       <Router>
-         <Switch>
-           <Route exact path="/user"
-                component={user}/>
-            <Route exact path="/admin"
-                component={admin}/>
 
-         </Switch>
+       <Router>
+          {/* <Link to='/'>Select User Type</Link> */}
+          <Link to='/user'>General Member</Link>
+          <Routes>
+            <Route path='/user' element={<User/>} />
+            {/* <Route path='/about' element={<About/>} /> */}
+          </Routes>
+         {/* <Switch>
+           <Route path="/user"
+                component={user}/>
+            <Redirect to="/" />
+         </Switch> */}
        </Router>
-        
+
       </header>
     </div>
   );
