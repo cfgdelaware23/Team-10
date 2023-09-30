0b95aa537e2f3c464e5e7677943a78b322370e88
Robel Kebede <robel.kebede@bison.howard.edu>
2023-09-30 02:31:32 -0400

    Added feature to send thank you email automatically

--- ACBproject/myapp/views.py
+++ ACBproject/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .forms import VolunteerForm, EventsForm
 from .models import Events, Volunteer, Registration
+import smtplib, ssl
 
 # Create your views here.
 def index(request):
@@ -18,16 +19,32 @@ def register_events(request):
     context = {'form':form}
     return render(request, 'register_events.html', context)
 
-# def volunteer(request):
-#     form = VolunteerForm()
-#     if request.method == 'POST':
-#         form = VolunteerForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#     context = {'form':form}
-#     return render(request, 'register_volunteer.html', context)
+def send_email(recipient):
+    sender_email = "acbcfg2023@gmail.com"
+    password = "jive koth yflp zdqi"
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    context = ssl.create_default_context()
 
-# Assuming you have a view where volunteers apply for events
+
+    subject = "ACB Update"
+    body = "Thank you so much for signing up! ACB appreciates you."
+    message = f"Subject: {subject}\n\n{body}"
+
+
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.starttls(context=context)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, recipient, message)
+
+
+        print("Email sent. ")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        server.quit()
+
 
 def volunteer(request):
     if request.method == 'POST':
@@ -43,16 +60,13 @@ def volunteer(request):
             if event_id:
                 event = Events.objects.get(pk=event_id)
                 
-                # if Volunteer.objects.filter(name=user, email=e).exists():
-                #     print(Volunteer.objects.filter(name=name, email=email).exists())
-                #     return HttpResponse("You are already registered for this event.")
-                
-                # if Registration.objects.filter(volunteer=user, event=event).exists():
-                #     print(Registration.objects.filter(volunteer=user, event=event))
-                #     return HttpResponse("You are already registered for this event.")
-                
                 Registration.objects.create(volunteer=user, event=event)
+                # Call to the email sending
+                email = volunteer.email
+                send_email(email)
+                # send_email(email)
                 return HttpResponse("Successfully applied for the event!")
+            
             return HttpResponse("Volunteer information saved successfully!")
 
     else:
@@ -60,52 +74,3 @@ def volunteer(request):
 
     context = {'form': form}
     return render(request, 'register_volunteer.html', context)
-
-# def volunteer(request):
-#     if request.method == 'POST':
-#         form = VolunteerForm(request.POST)
-#         if form.is_valid():
-#             # Save the volunteer information
-#             volunteer = form.save()
-
-#             # Check if there's an event_id in the POST data (assuming you pass it in your form)
-#             event_id = request.POST.get('event_id')
-
-#             if event_id:
-#                 # Get the event for which the volunteer is applying
-#                 event = Events.objects.get(pk=event_id)
-
-#                 # Check if the volunteer is not already registered for the event
-#                 if not Registration.objects.filter(volunteer=volunteer, event=event).exists():
-#                     # Create a new registration entry to associate the volunteer with the event
-#                     Registration.objects.create(volunteer=volunteer, event=event)
-#                     return HttpResponse("Successfully applied for the event!")
-
-#                 return HttpResponse("You are already registered for this event.")
-
-#             return HttpResponse("Volunteer information saved successfully!")
-
-#     else:
-#         form = VolunteerForm()
-
-#     context = {'form': form}
-#     return render(request, 'register_volunteer.html', context)
-
-# from django.shortcuts import get_object_or_404
-
-# def apply_for_event(request, event_id):
-#     # Get the volunteer who is applying (you might need to handle user authentication here)
-#     volunteer = get_object_or_404(Volunteer, id=request.user.id)
-
-#     # Get the event for which the volunteer is applying
-#     event = get_object_or_404(Events, id=event_id)
-
-#     # Check if the volunteer is not already registered for the event
-#     if not Registration.objects.filter(volunteer=volunteer, event=event).exists():
-#         # Create a new registration entry to associate the volunteer with the event
-#         Registration.objects.create(volunteer=volunteer, event=event)
-#         return HttpResponse("Successfully applied for the event!")
-
-#     return HttpResponse("You are already registered for this event.")
-
-# # You can create a URL pattern and link it to this view to handle volunteer applications.
