84f59045f285e2ca064abeca09df71de8f2b40a7
Robel Kebede <robel.kebede@bison.howard.edu>
2023-09-29 21:56:35 -0400

    Fix model field bug

--- ACBproject/myapp/models.py
+++ ACBproject/myapp/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-
+CHOICES = [
+    ('Host', 'Host'),
+    ('Moderator', 'Moderator'),
+    ('Facilitator', 'Facilitator'), 
+    ('Streamer', 'Streamer'), 
+    ('Broadcaster', 'Broadcaster'),
+]
 class Volunteer(models.Model):
     name = models.CharField(default=None, max_length=255)
     day = models.DateField(default=None, max_length=255)
-    roles = models.CharField(default=None, max_length=255)
+    roles = models.CharField(choices=CHOICES, default=None, max_length=255)
 
     def __str__(self) -> str:
         return f"Name: {self.name}, available day: {self.day}, roles: {self.roles}"
@@ -13,14 +19,15 @@ class Volunteer(models.Model):
 class Events(models.Model):
     recurring = models.BooleanField(default=None)
     day = models.DateField(default=None)
-    start_time = models.DateTimeField(default=None)
-    end_time = models.DateTimeField(default=None)
+    start_time = models.TimeField(default=None)
+    end_time = models.TimeField(default=None)
     title = models.CharField(default=None, max_length=255)
     account = models.CharField(default=None, max_length=255)
     host = models.CharField(default=None, max_length=255)
-    moderator = models.CharField(default=None, max_length=255)
-    facilitator = models.CharField(default=None, max_length=255)
-    broadcaster = models.CharField(default=None, max_length=255)
+    moderator = models.CharField(default=None, max_length=255, blank=True, null=True)
+    facilitator = models.CharField(default=None, max_length=255, blank=True, null=True)
+    streamer = models.CharField(default=None, max_length=255, blank=True, null=True)
+    broadcaster = models.CharField(default=None, max_length=255, blank=True, null=True)
 
     def __str__(self) -> str:
         return f"title: {self.title}, day: {self.day}, time: {self.time}, account: {self.account}"
