7850d17ff7ee79342b04f1512982dab4d33762f7
Robel Kebede <robel.kebede@bison.howard.edu>
2023-09-29 20:29:00 -0400

    Updated to char field instead of text field

--- ACBproject/myapp/models.py
+++ ACBproject/myapp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Volunteer(models.Model):
-    name = models.TextField(default=None)
-    day = models.DateField(default=None)
-    roles = models.TextField(default=None)
+    name = models.CharField(default=None, max_length=255)
+    day = models.DateField(default=None, max_length=255)
+    roles = models.CharField(default=None, max_length=255)
 
     def __str__(self) -> str:
         return f"Name: {self.name}, available day: {self.day}, roles: {self.roles}"
@@ -15,12 +15,12 @@ class Events(models.Model):
     day = models.DateField(default=None)
     start_time = models.DateTimeField(default=None)
     end_time = models.DateTimeField(default=None)
-    title = models.TextField(default=None)
-    account = models.TextField(default=None)
-    host = models.TextField(default=None)
-    moderator = models.TextField(default=None)
-    facilitator = models.TextField(default=None)
-    broadcaster = models.TextField(default=None)
+    title = models.CharField(default=None, max_length=255)
+    account = models.CharField(default=None, max_length=255)
+    host = models.CharField(default=None, max_length=255)
+    moderator = models.CharField(default=None, max_length=255)
+    facilitator = models.CharField(default=None, max_length=255)
+    broadcaster = models.CharField(default=None, max_length=255)
 
     def __str__(self) -> str:
         return f"title: {self.title}, day: {self.day}, time: {self.time}, account: {self.account}"
