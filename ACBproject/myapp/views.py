1068a51805145e65d886af246047fbe354c1f8cd
Andrew923 <yuandrew923@gmail.com>
2023-09-30 03:44:57 -0400

    added send email thingy

--- ACBproject/myapp/views.py
+++ ACBproject/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .forms import VolunteerForm, EventsForm
 from .models import Events, Volunteer, Registration
+import smtplib, ssl
 
 # Create your views here.
 
@@ -23,6 +24,32 @@ def register_events(request):
     context = {'form': form}
     return render(request, 'register_events.html', context)
 
+def send_email(recipient, event):
+    sender_email = "acbcfg2023@gmail.com"
+    password = "jive koth yflp zdqi"
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    context = ssl.create_default_context()
+
+
+    subject = "ACB Update"
+    body = f"Thank you so much for signing up for {event.title} from \
+{event.start_time} to {event.end_time} on {event.day}! ACB appreciates you."
+    message = f"Subject: {subject}\n\n{body}"
+
+
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.starttls(context=context)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, recipient, message)
+
+
+        print("Email sent. ")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        server.quit()
 
 def volunteer(request):
     if request.method == 'POST':
@@ -49,6 +76,8 @@ def volunteer(request):
                 #     return HttpResponse("You are already registered for this event.")
                 
                 Registration.objects.create(volunteer=user, event=event)
+                email = volunteer.email
+                send_email(email, event)
                 return HttpResponse("Successfully applied for the event!")
             return HttpResponse("Volunteer information saved successfully!")
 
