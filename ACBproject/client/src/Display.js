9af00f76d1d1ddf81d51bc91b96c211f53230cc1
Juskeerat Anand <juskeerat@gmail.com>
2023-09-29 21:34:25 -0400

    t deleted client

--- ACBproject/client/src/Display.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Display() {
-  return <h2>Display Page</h2>;
-}
-
-export default Display;
--- ACBproject/client/src/Members.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Members() {
-  return <h2>Members Page</h2>;
-}
-
-export default Members;
