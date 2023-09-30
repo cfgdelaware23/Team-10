120bb563024cf7453308deeb90fb3c669538a132
Robel Kebede <robel.kebede@bison.howard.edu>
2023-09-29 21:56:11 -0400

    Added dropdown feature

--- ACBproject/myapp/forms.py
+++ ACBproject/myapp/forms.py
@@ -1,13 +1,21 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput, TimeInput
 from .models import Volunteer, Events
 
 class VolunteerForm(ModelForm):
     class Meta:
         model = Volunteer
         fields = "__all__"
+        widgets = {
+            'day': DateInput(attrs={'type': 'date'}),
+        }
 
 
 class EventsForm(ModelForm):
     class Meta:
         model = Events
         fields = "__all__"
+        widgets = {
+            'day': DateInput(attrs={'type': 'date'}),
+            'start_time': TimeInput(attrs={'type': 'time'}),
+            'end_time': TimeInput(attrs={'type': 'time'}),
+        }
